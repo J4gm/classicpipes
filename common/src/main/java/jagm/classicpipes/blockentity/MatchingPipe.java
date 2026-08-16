4e43b79ff2abd499f95c638e474485a92509fe7c
J4gm <jack.a.g.mullin@outlook.com>
2026-08-16 18:00:02 +0100

    Routing Pipes directly attached to full containers reroute to default route. (close #95)
    (cherry picked from commit 15b355a558eb6cacb7d084a419ea19e9a6230ed5)

--- common/src/main/java/jagm/classicpipes/blockentity/MatchingPipe.java
+++ common/src/main/java/jagm/classicpipes/blockentity/MatchingPipe.java
@@ -2,15 +2,11 @@ package jagm.classicpipes.blockentity;
 
 import net.minecraft.world.item.ItemStack;
 
-public interface MatchingPipe {
+public interface MatchingPipe extends OverflowHandlingPipe {
 
     boolean matches(ItemStack stack);
 
     NetworkedPipeEntity getAsPipe();
-
-    void markCannotFit(ItemStack stack);
-
-    boolean itemCanFit(ItemStack stack);
 
     void updateCache();
 
